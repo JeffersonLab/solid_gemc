4c70500bbd730162d4c394d0c48e6148a206c78f
Zhiwen Zhao <zwzhao@lily.jlab.org>
2025-05-14 09:09:23 -0400

    fix error bug and update plot

--- analysis/Ay_FOM/ay_fom.C
+++ analysis/Ay_FOM/ay_fom.C
@@ -55,7 +55,7 @@ char *Q2_center[n]={"2.5","3.5","4.5","5.5","6.5","7.5","8.5","9.5"};
 double count[n]={2.97546e8,3.15e9,4.0e9,2.9572e9,1.31311e9,3.84377e8,6.77e7,1.27e6};  //P>3.0GeV, rate=total/3600/24/48=2.9khz
 
 //Assumption
-double Ay=-0.01; // assume Ay=-0.01
+double Ay=-0.01; // same assumption as in proposal
 double Pt=0.6; //He3 polarization
 double Pn=0.86; //effective neutron polarization
 double dhe3=0.85; //He3 dilution
@@ -73,21 +73,21 @@ for(int i=0;i<n;i++){
    h_Aut[i]=new TH1F(hstname,Form("Q^{2}=%sGeV^{2};#phi (rad);A_{UT}",Q2_center[i]),12,0,3.1416*2);
    
    for(int j=0;j<12;j++){
-    double error_stat=1/sqrt(count[i]/12.)/Pt/Pn/dhe3/dn; //relative stat error, average in 12 phi bins
-    double error_sys=0.07; //relative sys error according to proposal
-    double error=error_stat; // not using sys error yet
-    double content=Ay*sin(3.1416*2/12*(j+0.5)); // with simple sin form
-//     double content_smeared=content; // no smearing
-    double content_smeared=gRandom->Gaus(content,content*error); // yes smearing
-       
-    h_Aut[i]->SetBinContent(j+1,content_smeared);
-    h_Aut[i]->SetBinError(j+1,error*content); 
+    double Aut=Ay*sin(3.1416*2/12*(j+0.5)); // with simple sin form
+    double Aut_error_stat=1/sqrt(count[i]/12.)/Pt/Pn/dhe3/dn; //abs stat error, average in 12 phi bins
+    double Aut_error_sys=0.07*Aut+1e-4+1e-3; //abs sys error according to proposal
+    double Aut_error_total=Aut_error_stat; // not using sys error yet
+    double Aut_smeared=gRandom->Gaus(Aut,Aut_error_total); // smearing to prevent overfitting
+   
+//     h_Aut[i]->SetBinContent(j+1,Aut); 
+    h_Aut[i]->SetBinContent(j+1,Aut_smeared);
+    h_Aut[i]->SetBinError(j+1,Aut_error_total); 
    }
    
    c_Aut->cd(i+1);
    h_Aut[i]->SetMaximum(0.02);
    h_Aut[i]->SetMinimum(-0.02);   
-   h_Aut[i]->Draw("E1");
+   h_Aut[i]->Draw("E1 X0");
    
    TF1 *f = new TF1("f", "[0]*sin(x)", 0, 10);
    f->SetParameter(0,-0.01); 
@@ -99,27 +99,33 @@ for(int i=0;i<n;i++){
    pt->AddText(Form("Ay %f #pm %f",Ay_fit,Ay_error));
    pt->Draw();
    
-   h_Ay->SetBinContent(i+2,Ay);
-   h_Ay->SetBinError(i+2,Ay_error);
+   if (i!=7){
+    h_Ay->SetBinContent(i+2,Ay);
+    h_Ay->SetBinError(i+2,Ay_error);
+   }
 }
 
 TCanvas *c_Ay = new TCanvas("c_Ay","c_Ay",1000,800);
    
-h_Ay->SetMaximum(-0.01+0.002);
-h_Ay->SetMinimum(-0.01-0.002);
+h_Ay->SetMaximum(-0.01+0.03);
+h_Ay->SetMinimum(-0.01-0.03);
+// h_Ay->SetMaximum(-0.01+0.004);
+// h_Ay->SetMinimum(-0.01-0.004);
 h_Ay->SetMarkerColor(kRed);
 h_Ay->SetLineColor(kBlack);
-h_Ay->Draw("E1");
+h_Ay->Draw("E1 X0");
 
-double Ay_error_proposal[9]={1.47e-5,2.58e-5,4.88e-5,8.57e-5,1.27e-4,2.04e-4,4.11e-4,1.07e-3,8.68e-3}; // at He3 11GeV, Fig 33 and Fig 37 of proposal
+// He3 11GeV, Fig 33 and Fig 37 of proposal
+// double Ay_error_proposal[9]={1.47e-5,2.58e-5,4.88e-5,8.57e-5,1.27e-4,2.04e-4,4.11e-4,1.07e-3,8.68e-3};
+double Ay_error_proposal[8]={1.47e-5,2.58e-5,4.88e-5,8.57e-5,1.27e-4,2.04e-4,4.11e-4,1.07e-3};  
 TH1F* h_Ay_proposal=new TH1F("Ay_proposal","SoLID He3 11GeV projection;Q^{2} (GeV^{2});A_{y}",9,1.1,10.1);
-for(int i=0;i<9;i++){
+for(int i=0;i<8;i++){
 h_Ay_proposal->SetBinContent(i+1,Ay);
 h_Ay_proposal->SetBinError(i+1,Ay_error_proposal[i]);
 }
 h_Ay_proposal->SetMarkerColor(kRed);
 h_Ay_proposal->SetLineColor(kRed);
-h_Ay_proposal->Draw("same E1");
+h_Ay_proposal->Draw("same E1 X0");
 
 Double_t x[4]  = {3.24,2.56,2.08,1.58};
 Double_t y[4]  = {-3.87e-2,-3.89e-2,-1.08e-2,-3.84e-2};  
@@ -128,12 +134,12 @@ Double_t ey[4] = {1.55e-2,0.96e-2,1.18e-2,2e-2};
 TGraphErrors *gr = new TGraphErrors(4,x,y,ex,ey);
 gr->SetMarkerColor(kBlue);
 gr->SetMarkerStyle(21);
-// gr->Draw("P");
+gr->Draw("P");
 
 TLegend* leg = new TLegend(0.6, 0.8, 0.9, 0.9);
 leg->AddEntry(h_Ay,Form("LD with full trigger"),"l");
 leg->AddEntry(h_Ay_proposal,Form("FD+LD with prescaled trigger in proposal"),"l");
-// leg->AddEntry(gr,Form("PRL"),"l");
+leg->AddEntry(gr,Form("published PRL result"),"l");
 leg->Draw();
 
 }
